bea4a2d18641a588626e435968c79c7a9aef7fe2
Doug Pfeffer <dougpfeffer2013@gmail.com>
2020-07-23 18:17:53 -0700

    JS Functions

--- js/app.js
+++ js/app.js
@@ -1,50 +1,94 @@
 'use strict';
 
-var name = prompt('What is your name?');
-console.log('Hello, welcome to my page,', name);
-alert('Hello! Welcome to my page, ' + name + '.');
-
-var answer1 = confirm('Am I from a foreign country?');
-if (answer1 === true) {
-  console.log('that\'s right, I\'m from Canada!');
-  alert('that\'s right, I\'m from Canada!');
-} else {
-  console.log('Nope, you ungrateful colonists, I\'m from Canada!');
-  alert('Nope, you ungrateful colonists, I\'m from Canada!');
+askName();
+function askName() {
+  var name = prompt('What is your name?');
+  // console.log('Hello, welcome to my page,', + name);
+  alert('Hello! Welcome to my page, ' + name + '. My name is Doug. Let\s play a game. I\'m going to ask you five questions, to see if you really know me.');
 }
 
-var answer2 = confirm('Am I a Hockey Fan?');
-if (answer1 === true) {
-  console.log('absolutely correct, I\'m from Canada!');
-  alert('absolutely correct, I\'m from Canada!');
-} else {
-  console.log('WRONG ANSWER! I\'m from Canada!');
-  alert('WRONG ANSWER! I\'m from Canada!');
+// Function 1
+
+askAnswer1();
+function askAnswer1() {
+  var answer1 = prompt('Am I from a foreign country?', 'Type yes or no.').toLowerCase();
+
+  while (answer1 !== 'yes' && answer1 !== 'no' && answer1 !== 'y' && answer1 !== 'n') {
+    answer1 = prompt('Please answer yes or no....nothing else');
+  }
+  if (answer1 === 'yes' || answer1 === 'y') {
+    // console.log('That\'s right, I\'m from Canada!');
+    alert('That\'s right, I\'m from Canada!');
+  } else if (answer1 === 'no' || answer1 === 'n') {
+    // console.log('Nope, you ungrateful colonists, I\'m from Canada!');
+    alert('Nope, you ungrateful colonists, I\'m from Canada!');
+  }
 }
 
-var answer3 = confirm('Do I like Poutine?');
-if (answer1 === true) {
-  console.log('Of course I do, I\'m from Canada!');
-  alert('Of course I do, I\'m from Canada!');
-} else {
-  console.log('WRONG ANSWER! I\'m from Canada!');
-  alert('WRONG ANSWER! I\'m from Canada!');
+// Function 2
+
+askAnswer2();
+function askAnswer2() {
+  var answer2 = prompt('Am I a Hockey Fan?', 'Type yes or no.').toLowerCase();
+  while (answer2 !== 'yes' && answer2 !== 'no' && answer2 !== 'y' && answer2 !== 'n') {
+    answer2 = prompt('Please answer yes or no....nothing else');
+  }
+  if (answer2 === 'yes' || answer2 === 'y') {
+    // console.log('absolutely correct, I\'m from Canada!');
+    alert('Absolutely correct, I\'m from Canada!');
+  } else if (answer2 === 'no' || answer2 === 'n') {
+    // console.log('WRONG ANSWER! I\'m from Canada!');
+    alert('WRONG ANSWER! I\'m from Canada!');
+  }
 }
 
-var answer4 = confirm('Do I watch Curling?');
-if (answer1 === true) {
-  console.log('Of course I do, I\'m from Canada!');
-  alert('Of course I do, I\'m from Canada!');
-} else {
-  console.log('WRONG ANSWER! I\'m from Canada!');
-  alert('WRONG ANSWER! I\'m from Canada!');
+// Function 3
+
+askAnswer3();
+function askAnswer3() {
+  var answer3 = prompt('Do I like Poutine?', 'Type yes or no.');
+  while (answer3 !== 'yes' && answer3 !== 'no' && answer3 !== 'y' && answer3 !== 'n') {
+    answer3 = prompt('Please answer yes or no....nothing else');
+  }
+  if (answer3 === 'yes' || answer3 === 'y') {
+    // console.log('Of course I do, I\'m from Canada!');
+    alert('Of course I do, I\'m from Canada!');
+  } else if (answer3 === 'no' || answer3 === 'n') {
+    // console.log('WRONG ANSWER! I\'m from Canada!');
+    alert('WRONG ANSWER! I\'m from Canada!');
+  }
 }
 
-var answer5 = confirm('Do I listen to Rush?');
-if (answer1 === true) {
-  console.log('Nope, just because I\'m from Canada, doesn\'t mean that I like Rush!');
-  alert('No, just because I\'m from Canada, doesn\'t mean that I like Rush!');
-} else {
-  console.log('WRONG ANSWER! I\'m from Canada!');
-  alert('WRONG ANSWER! I\'m from Canada!');
+// Function 4
+
+askAnswer4();
+function askAnswer4() {
+  var answer4 = prompt('Do I watch Curling?');
+  while (answer4 !== 'yes' && answer4 !== 'no' && answer4 !== 'y' && answer4 !== 'n') {
+    answer4 = prompt('Please answer yes or no....nothing else');
+  }
+  if (answer4 === 'yes' || answer4 === 'y') {
+    // console.log('Of course I do, I\'m from Canada!');
+    alert('Of course I do, I\'m from Canada!');
+  } else if (answer4 === 'no' || answer4 == 'n') {
+    // console.log('WRONG ANSWER! I\'m from Canada!');
+    alert('WRONG ANSWER! I\'m from Canada!');
+  }
+}
+
+// Function 5
+
+askAnswer5();
+function askAnswer5() {
+  var answer5 = prompt('Do I listen to Rush?');
+  while (answer5 !== 'yes' && answer5 !== 'no' && answer5 !== 'y' && answer5 !== 'n') {
+    answer5 = prompt('Please answer yes or no....nothing else');
+  }
+  if (answer5 === 'yes' || answer5 === 'y') {
+    // console.log('Nope, just because I\'m from Canada, doesn\'t mean that I like Rush!');
+    alert('Nope, just because I\'m from Canada, doesn\'t mean that I like Rush!. Thanks for playing ' + name + '.');
+  } else if (answer5 === 'no' || answer5 === 'n') {
+    // console.log('You are correct! Just because I\'m from Canada, doesn\'t mean that I like Rush...they suck!');
+    alert('You are correct! Just because I\'m from Canada, doesn\'t mean that I like Rush...they suck! Thanks for playing ' + name + '.');
+  }
 }
